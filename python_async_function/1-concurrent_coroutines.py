1e14ef2f352472ce15a90e8ac0b009bb3eb9bd2e
root <root@DESKTOP-OFAJ8QV.localdomain>
2023-08-29 14:05:28 -0300

    task 1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -10,6 +10,6 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     "ou will spawn wait_random n times with the specified max_delay."
     list_floats = []
     for i in range(n):
-        delay =  await (wait_random(max_delay))
+        delay = await (wait_random(max_delay))
         list_floats.append(delay)
     return sorted(list_floats)
