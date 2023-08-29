17e601df0ae762518c6a0186cf9901ce961a6449
root <root@DESKTOP-OFAJ8QV.localdomain>
2023-08-29 10:29:02 -0300

    task 2

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -2,6 +2,6 @@
 """Write a type-annotated function floor"""
 
 
-def floor(n: float) -> float:
+def floor(n: float) -> int:
     "takes a float n as argument and returns the floor"
     return int(n)
