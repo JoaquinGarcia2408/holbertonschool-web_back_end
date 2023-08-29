e9b7d208f17faf711198eb087efaf3a2ad6bd7b4
root <root@DESKTOP-OFAJ8QV.localdomain>
2023-08-29 10:21:43 -0300

    errores task 7

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-"""Write a type-annotated function to_kv that takes a string k and an int OR float v"""
-from typing import List, Union
+"Write a type-annotated function to_kv that takes a string and an int OR float"
+from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[float, int]) -> tuple[str, float]:
+def to_kv(k: str, v: Union[float, int]) -> Tuple[str, float]:
     "arguments and returns a tuple"
     return (k, float(v ** 2))
