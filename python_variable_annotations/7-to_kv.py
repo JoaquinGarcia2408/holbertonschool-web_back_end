474f730cbafbdf590c95f307ca90ec956f7b9be9
root <root@DESKTOP-OFAJ8QV.localdomain>
2023-08-29 10:35:33 -0300

    task 7

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"Write a type-annotated function to_kv that takes a string and an int OR float"
-from typing import Tuple, Union
+"Task 7: to_kv() function"
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[float, int]) -> Tuple[str, float]:
-    "arguments and returns a tuple"
-    tup = (k, v * v)
-    return tup
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """takes a string and an int/float and returns
+    them into a tuple"""
+    return k, v**2
