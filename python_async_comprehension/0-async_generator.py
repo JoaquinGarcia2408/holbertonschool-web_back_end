0a67c61fe29156a54ff684a76bd492b582477f90
root <root@DESKTOP-OFAJ8QV.localdomain>
2023-09-01 13:01:31 -0300

    task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,6 +5,7 @@ import random
 
 
 async def async_generator():
+    "The coroutine will loop 10 times, each time asynchronously wait 1 second"
     for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
