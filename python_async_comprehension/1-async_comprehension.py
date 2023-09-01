90904421e19f8709fda39c0123b079efe76cfaea
root <root@DESKTOP-OFAJ8QV.localdomain>
2023-09-01 14:12:58 -0300

    task 1

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"task 1: Async Comprehensions"
+import asyncio
+import random
+async_generator = __import__("0-async_generator").async_generator
+
+
+async def async_comprehension():
+    random_numbers = [number async for number in async_generator()]
+    return random_numbers
