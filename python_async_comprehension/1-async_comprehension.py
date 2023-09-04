f42d6c393341b4196c2ca285ad3f310478fda85f
root <root@DESKTOP-OFAJ8QV.localdomain>
2023-09-03 22:02:29 -0300

    task 1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -2,10 +2,11 @@
 "task 1: Async Comprehensions"
 import asyncio
 import random
+from typing import List
 async_generator = __import__("0-async_generator").async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     "then return the 10 random numbers."
     random_numbers = [number async for number in async_generator()]
     return random_numbers
