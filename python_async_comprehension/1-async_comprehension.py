ce2adb1d10367d92b99f114e7a480b046132b616
root <root@DESKTOP-OFAJ8QV.localdomain>
2023-09-03 21:57:35 -0300

    task 2

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -6,5 +6,6 @@ async_generator = __import__("0-async_generator").async_generator
 
 
 async def async_comprehension():
+    "The coroutine will collect 10 random numbers using an async comprehensing over async_generator"
     random_numbers = [number async for number in async_generator()]
     return random_numbers
