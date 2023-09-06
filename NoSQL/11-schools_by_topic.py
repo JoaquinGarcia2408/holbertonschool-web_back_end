12f8305ffc36a161c85f67f408376e4a956ff9ef
JoaquinGarcia2408 <joacogarcia2408@gmail.com>
2023-09-06 06:24:55 -0700

    task 11

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -6,6 +6,6 @@ def schools_by_topic(mongo_collection, topic):
     "function that returns the list of school having a specific topic:"
     cursor = mongo_collection.find({"topics": topic})
     school= []
-    for i in school:
+    for i in cursor:
         school.append(i)
     return school
