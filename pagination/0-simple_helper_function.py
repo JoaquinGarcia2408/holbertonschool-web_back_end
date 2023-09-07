19d02d13a0ef6174491956f13576d62907bc5f2c
root <root@DESKTOP-OFAJ8QV.localdomain>
2023-09-07 12:19:18 -0300

    task 0

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -6,5 +6,5 @@ def index_range(page, page_size):
     "start index and an end index corresponding to the range of indexes"
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
-    
+
     return (start_index, end_index)
