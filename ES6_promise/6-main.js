846eed5a3b980c195f0524e9105fede6c0fd6113
Joaquin <joacogarcia2408@gmail.com>
2023-10-05 11:00:36 -0700

    task 6

--- ES6_promise/6-main.js
+++ ES6_promise/6-main.js
@@ -1,3 +1,5 @@
 import handleProfileSignup from './6-final-user';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 console.log(handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg"));
