ea2fbd93d674de4fad7340407b93271bf3a48b9b
Joaquin <joacogarcia2408@gmail.com>
2023-10-09 09:11:30 -0700

    task 4 eslint

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,11 @@
 export default function updateStudentGradeByCity(listStudents, city, newGrades) {
-  const filteredStudents = listStudents.filter(student => student.location === city);
-  const updatedStudents = filteredStudents.map(student => {
-    const newGrade = newGrades.find(grade => grade.studentId === student.id);
+  const filteredStudents = listStudents.filter((student) => student.location === city);
+  const updatedStudents = filteredStudents.map((student) => {
+    const newGrade = newGrades.find((grade) => grade.studentId === student.id);
     return {
       ...student,
       grade: newGrade ? newGrade.grade : 'N/A',
     };
   });
   return updatedStudents;
-};
+}
