f088dbbefac4bc6f1db90f11df91c7be56713dfa
Joaquin <joacogarcia2408@gmail.com>
2023-10-10 07:26:23 -0700

    task 8

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,13 @@
+export default function cleanSet(set, startString) {
+  if (startString === '' || !startString) {
+    return ''
+  }
+  if (typeof startString !== 'string') {
+    return ''
+  }
+  const arrayFromSet = Array.from(set);
+  const filteredValues = arrayFromSet
+    .filter(value => value.startsWith(startString))
+    .map(value => value.slice(startString.length));
+  return filteredValues.join('-');
+}
